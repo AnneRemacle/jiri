e6dd9e575743a19dbeddf66525103434d74f5ece
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-22 12:04:16 +0200

    UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Jiri\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Route;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function events(Request $request){
+        $apiRequest = Request::create('/api/users/'.Auth::user()->id.'/events', 'GET');
+        $request->replace($apiRequest->input());
+        $events = Route::dispatch($apiRequest)->getOriginalContent();
+
+        return view("users.events")->with([
+            "events" => $events,
+            "user" => Auth::user()
+        ]);
+    }
+}
