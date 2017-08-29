148c8522d3d7ea1a329d63560e2cb450acf5ad8b
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-29 14:46:46 +0200

    ProjectController

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Jiri\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Jiri\Event;
+use Jiri\Project;
+use Jiri\Weight;
+
+class ProjectController extends Controller
+{
+    public function create( Event $event, Project $project ) {
+        $projects = Project::all();
+
+        return view("projects.create")->with([
+            "event" => $event,
+            "projects" => $projects
+        ]);
+    }
+
+    public function addOrStore( Event $event, Request $request ){
+
+        if( Project::where("name", $request->get("name"))->count() ){
+            $project = Project::where("name", $request->get("name"))->first();
+        }else{
+            $project = Project::create([
+                "name" => $request->get("name"),
+                "description" => $request->get("description")
+            ]);
+            $project->save();
+        }
+
+        $weight = Weight::create([
+            "weight" => $request->get("weight")
+        ]);
+
+        $weight->project()->associate($project);
+        $weight->event()->associate($event);
+        $weight->save();
+
+        return redirect()->back();
+    }
+}
