f27ae73c022fd15ce4d8884d0e4f69689ceae0d3
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-31 10:50:39 +0200

    Including event in dashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,17 +6,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Jiri\Event;
 
 class DashboardController extends Controller
 {
     public function main(Request $request)
     {
-        $apiRequest = Request::create('/api/events/1/students', 'GET', ['embed' => 'performances']);
-        $request->replace($apiRequest->input());
-        $event = Route::dispatch($apiRequest)->getOriginalContent();
-        $user = Auth::user();
+        $events = Event::whereHas("meetings", function($query) {
+            $query->where("user_id", Auth::user()->id);
+        })->orderBy("created_at", "DESC")->get();
 
-        return view('users/dashboard', compact('event', 'user'));
+        return view('users/dashboard')->with([
+            "user" => Auth::user(),
+            "events" => $events
+        ]);
     }
 
     public function admin(Request $request) {
