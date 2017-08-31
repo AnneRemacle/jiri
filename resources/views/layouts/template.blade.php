535b302c092c63356627d174643622b20c7b4327
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-31 10:57:31 +0200

    Adding css file in main template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,8 +9,9 @@
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/flatpickr"></script>
-    
+
     <script src="{{ asset("js/main.js") }}" type="text/javascript"></script>
+    <link rel="stylesheet" href="{{ asset("css/styles.css") }}">
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
