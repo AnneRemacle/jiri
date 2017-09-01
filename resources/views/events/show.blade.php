b8df6df7ab8eb35f44dd5a63d302946378349740
Anne Remacle <anne.m.remacle@gmail.com>
2017-09-01 12:43:49 +0200

    Addind a class to have bordered cells

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -18,10 +18,8 @@
             @endif
         </nav>
 
-
-
         @if( $event->meetings->count() )
-            <table class="table">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th></th>
