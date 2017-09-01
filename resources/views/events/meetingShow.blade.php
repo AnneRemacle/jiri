7d29d6321ffa29f4a82714a7c2f9408b9d29c3f6
Anne Remacle <anne.m.remacle@gmail.com>
2017-09-01 12:45:36 +0200

    Show a meeting

--- resources/views/events/meetingShow.blade.php
+++ resources/views/events/meetingShow.blade.php
@@ -4,14 +4,30 @@
 @include('layouts.header')
 @section( "content" )
     <section class="container">
-        <div class="row">
-            <h2 class="page-header">{{ $meeting->student->name }}</h2>
-            <ul>
-                @foreach( $meeting->student->implementations->where("event_id", $event->id) as $implementation )
-                    <li>{{ $implementation->project->name }} </li>
-                @endforeach
-            </ul>
-        </div>
+        <h2 class="page-header title">Rencontre avec {{ $meeting->student->name }}</h2>
+
+        <section>
+            <h3>Projets à évaluer</h3>
+            @foreach( $meeting->student->implementations->where("event_id", $event->id) as $implementation )
+                <section class="single-project">
+                    <h4>
+                        <a data-toggle="collapse" data-parent="#accordion" href="#project-{{$implementation->project->id}}" class="title-collapse" >
+                        {{ $implementation->project->name }}
+                        </a>
+                    </h4>
+                    <div id="project-{{$implementation->project->id}}" class="collapse single-event__content">
+                        <div class="links">
+                            <a href="{{ $implementation->project->url_repo }}" class="links__single">Voir le repo github</a>
+                            <a href="{{ $implementation->project->url_project }}" class="links__single">Voir le projet</a>
+                        </div>
+
+                        @include( "scores.forms.update" )
+                    </div>
+                </section>
+
+            @endforeach
+        </section>
+
     </section>
 @endsection
 
